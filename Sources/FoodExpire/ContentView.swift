6198f81107bef3164d622ea70e33c18aa0fdab77
itomonokata <81869808+imk0512@users.noreply.github.com>
2025-06-24 13:26:00 +0900

    Add initial SwiftUI package with Firebase dependencies

---
+++ Sources/FoodExpire/ContentView.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("FoodExpire Base Project")
+            .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
